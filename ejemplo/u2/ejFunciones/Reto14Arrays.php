495b71a4462a6ff082a7ce7ee4d2239b653df157
adrihm16 <ahermor2608@g.educaand.es>
2025-11-07 15:39:35 +0100

    actualizo ejercicio 14 retos arrays

--- ejemplo/u2/ejFunciones/Reto14Arrays.php
+++ ejemplo/u2/ejFunciones/Reto14Arrays.php
@@ -46,24 +46,70 @@
             ]
     ];
 
-    echo "<table border='1'>";
-    foreach ($almacen as $nombrePasillo => $pasillo) {
-        echo "<tr><th colspan='3'>$nombrePasillo</th></tr>";
-        foreach ($pasillo as $estanteria => $niveles) {
-            echo "<tr><td colspan='3'>Estantería $estanteria</td></tr>";
-            foreach ($niveles as $nivel => $productos) {
-                echo "<tr><td colspan='3'>$nivel</td></tr>";
-                foreach ($productos as $producto) {
-                    echo "<tr>";
-                    echo "<td>{$producto['nombre']}</td>";
-                    echo "<td>{$producto['cantidad']}</td>";
-                    echo "<td>{$producto['caducidad']}</td>";
-                    echo "</tr>";
+    /**
+     * @param array $almacen
+     * @return void
+     */
+    function mostrarTabla(array $almacen): void
+    {
+        echo "<table border='1'>";
+        foreach ($almacen as $nombrePasillo => $pasillo) {
+            echo "<tr><th colspan='3'>$nombrePasillo</th></tr>";
+            foreach ($pasillo as $estanteria => $niveles) {
+                echo "<tr><td colspan='3'>Estantería $estanteria</td></tr>";
+                foreach ($niveles as $nivel => $productos) {
+                    echo "<tr><td colspan='3'>$nivel</td></tr>";
+                    foreach ($productos as $producto) {
+                        echo "<tr>";
+                        echo "<td>{$producto['nombre']}</td>";
+                        echo "<td>{$producto['cantidad']}</td>";
+                        echo "<td>{$producto['caducidad']}</td>";
+                        echo "</tr>";
+                    }
                 }
             }
         }
+        echo "</table>";
     }
-    echo "</table>";
+    mostrarTabla($almacen);
+    /**
+     * @param array $almacen
+     * @return void
+     */
+    function contarProductosPorPasillo(array $almacen): void
+    {
+        echo "<h3>Productos Por Pasillo</h3>";
+        $productosPasillo = 0;
+        foreach ($almacen as $nombrePasillo => $pasillo) {
+            $productosPasillo = 0;
+            foreach ($pasillo as $estanteria => $niveles) {
+                foreach ($niveles as $nivel => $productos) {
+                    $productosPasillo += count($productos);
+                }
+            }
+            echo "$nombrePasillo: $productosPasillo productos <br>";
+        }
+    }
+    contarProductosPorPasillo($almacen);
+
+    echo "<h3>Productos Que Caducan Antes de 2028</h3>";
+    $añoCaducidad = 2030;
+    $productosPasillo = 0;
+    foreach ($almacen as $nombrePasillo => $pasillo) {
+        $productosPasillo = 0;
+        $añoCaducidad = 2030;
+        foreach ($pasillo as $estanteria => $niveles) {
+            foreach ($niveles as $nivel => $productos) {
+                foreach ($productos as $producto) {
+                    $añoCaducidad = substr($producto['caducidad'], 0, 4 );
+                if ($añoCaducidad <2028) {
+                    echo "<pre>{$producto['nombre']}\t{$producto['caducidad']}</pre>";
+                }
+                }
+            }
+        }
+
+    }
 ?>
 </body>
 </html>
